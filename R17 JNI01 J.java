b5dd2f8dae2936f15769578e4be8b72a4af362b2
omarsaghorchi <54277341+omarsaghorchi@users.noreply.github.com>
2025-03-02 16:47:16 -0500

    Update R17 JNI01 J.java

--- R17 JNI01 J.java
+++ R17 JNI01 J.java
@@ -1,30 +1,42 @@
-// Trusted.java
+ // Trusted.java
  
 import java.security.*;
  
 public class Trusted {
  
-   public static void loadLibrary(final String library){
-      AccessController.doPrivileged(new PrivilegedAction<Void>() {
-         public Void run() {
-             System.loadLibrary(library);
-             return null;
-         }
-      });
+   // load native libraries
+   static{
+      System.loadLibrary("NativeMethodLib1");
+      System.loadLibrary("NativeMethodLib2");
+      ...
    }
-}
  
----------------------------------------------------------------------------------
+   // private native methods
+   private native void nativeOperation1(byte[] data, int offset, int len);
+   private native void nativeOperation2(...)
+   ...
+  
+   // wrapper methods perform SecurityManager and input validation checks
+   public void doOperation1(byte[] data, int offset, int len) {
+      // permission needed to invoke native method
+      securityManagerCheck();
  
-// Untrusted.java
+      if (data == null) {
+         throw new NullPointerException();
+      }
  
-public class Untrusted {
+      // copy mutable input
+      data = data.clone();
  
-   private native void nativeOperation();
+      // validate input
+      if ((offset < 0) || (len < 0) || (offset > (data.length - len))) {
+         throw new IllegalArgumentException();
+      }
  
-   public static void main(String[] args) {
-      String library = new String("NativeMethodLib");
-      Trusted.loadLibrary(library);
-      new Untrusted.nativeOperation();  // invoke the native method
+      nativeOperation1(data, offset, len);
+   }
+    
+   public void doOperation2(...){
+      ...
    }
 }
